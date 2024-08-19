e3374102021f8fe0e370cb2229de4bf6ccb25799
shriramBaNW <72285290+Shriram-Vasudevan@users.noreply.github.com>
2024-08-19 17:36:12 -0400

    Rebuilding Exercise Library

--- LevelUpFitness/LevelUpFitness/Models/ExerciseLibraryExercise.swift
+++ LevelUpFitness/LevelUpFitness/Models/ExerciseLibraryExercise.swift
@@ -27,4 +27,27 @@ struct Progression: Codable, Hashable, Equatable {
     var level: Int
     var cdnURL: String
     var exerciseType: String
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: DynamicKey.self)
+        name = try container.decodeIfPresent(String.self, forKey: DynamicKey(stringValue: "Name")!) ?? ""
+        description = try container.decodeIfPresent(String.self, forKey: DynamicKey(stringValue: "Description")!) ?? ""
+        level = try container.decodeIfPresent(Int.self, forKey: DynamicKey(stringValue: "Level")!) ?? 0
+        cdnURL = try container.decodeIfPresent(String.self, forKey: DynamicKey(stringValue: "CDNURL")!) ?? ""
+        exerciseType = try container.decodeIfPresent(String.self, forKey: DynamicKey(stringValue: "ExerciseType")!) ?? ""
+    }
+}
+
+struct DynamicKey: CodingKey {
+    var stringValue: String
+    var intValue: Int?
+    
+    init?(stringValue: String) {
+        self.stringValue = stringValue
+    }
+    
+    init?(intValue: Int) {
+        self.intValue = intValue
+        self.stringValue = "\(intValue)"
+    }
 }
